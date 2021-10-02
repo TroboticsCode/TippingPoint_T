47d7addb49729537a41119a5e7e22e9341aee817
TroboticsCode <54865827+TroboticsCode@users.noreply.github.com>
2021-10-02 09:07:10 -0400

    Initial commit

---
+++ include/robot-config.h
@@ -0,0 +1,22 @@
+#ifndef ROBOT_CONFIG_H
+#define ROBOT_CONFIG_H
+
+using namespace vex;
+
+extern brain Brain;
+
+// VEXcode devices - declare new devices here, set them up
+// in robot-config.cpp
+extern controller Controller1;
+extern motor winch;
+extern motor arm;
+
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Text.
+ * 
+ * This should be called at the start of your int main function.
+ */
+void  vexcodeInit( void );
+
+#endif
---
+++ src/robot-config.cpp
@@ -0,0 +1,26 @@
+#include "vex.h"
+#include "DriveFunctionsConfig.h"
+
+using namespace vex;
+using signature = vision::signature;
+using code = vision::code;
+
+// A global instance of brain used for printing to the V5 Brain screen
+brain  Brain;
+
+// VEXcode device constructors
+controller Controller1 = controller(primary);
+motor arm = motor(PORT17, GREEN, true);
+motor winch = motor(PORT11, GREEN, false);
+
+// VEXcode generated functions
+
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Text.
+ * 
+ * This should be called at the start of your int main function.
+ */
+void vexcodeInit( void ) {
+  // nothing to initialize
+}
